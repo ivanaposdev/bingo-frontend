dfc8661f1b502742c1a288dcb9f4fbb9035a54df
Ivan Apos <aposivanrodger@gmail.com>
2023-10-10 23:55:14 +0800

    tweaking the default configurations

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
@@ -16,5 +16,12 @@ const config: Config = {
     },
   },
   plugins: [],
-}
-export default config
+};
+
+module.exports = {
+  corePlugins: {
+    preflight: false,
+  },
+};
+
+export default config;
